51d99b33a07f89576a7f676e7f15683cb3c51fcc
Chloe-O <chloeosbourne@yahoo.co.uk>
2021-01-15 11:28:34 +0000

    run.py file ud, testadded to index to ensure content is pushed to Heroku

--- run.py
+++ run.py
@@ -5,8 +5,6 @@ if os.path.exists("env.py"):
     import env
 
 
-# 2 spaces between functions for PEP8 compliance #
-
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 
@@ -38,7 +36,6 @@ def register():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=True)
-        
