53a52bfb1d47985ef03309df1ba57c69b392efc4
Chloe-O <chloeosbourne@yahoo.co.uk>
2021-01-15 12:44:17 +0000

    MongoDB connected, recipe collection added to recipe page

--- run.py
+++ run.py
@@ -1,12 +1,21 @@
 # Flask class imported #
 import os
-from flask import Flask, render_template, request, flash
+from flask import (
+    Flask, render_template, request, 
+    flash, redirect, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -15,8 +24,10 @@ def index():
 
 
 @app.route("/recipes")
+@app.route("/get_recipes")
 def recipes():
-    return render_template("recipes.html")
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
 
 
 @app.route("/login", methods=["GET", "POST"])
