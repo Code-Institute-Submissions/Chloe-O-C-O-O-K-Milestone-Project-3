15bfd68f9160c03bfbbf0b4aba99f5126da7fb80
Chloe-O <chloeosbourne@yahoo.co.uk>
2021-01-21 07:58:44 +0000

    profile page and redirect functions added

--- run.py
+++ run.py
@@ -55,6 +55,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Registeration complete!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -71,6 +72,7 @@ def login():
                existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome back {}!".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
                 # Password doesn't match, redirects to login page#
                 flash("Password incorrect, please try again")
@@ -84,6 +86,12 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # User's username is taken from DB and displays on page#
+    username = mongo.db.users.find.one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 # change to false before submitting #
 
 
