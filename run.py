c2e94629c3e46aeef2a9011d5c1279b6de37bd49
Chloe-O <chloeosbourne@yahoo.co.uk>
2021-01-22 21:27:28 +0000

    jinja loop fixed, accordion fixed

--- run.py
+++ run.py
@@ -33,7 +33,7 @@ def index():
 
 @app.route("/recipes")
 def recipes():
-    recipes = mongo.db.recipes.find()
+    recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
 
