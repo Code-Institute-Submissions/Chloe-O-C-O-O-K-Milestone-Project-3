0baef6b0395bee42b0a3a9c1ce32aebfd8ff4dcc
Chloe-O <chloeosbourne@yahoo.co.uk>
2021-03-17 20:59:52 +0000

    shadow added under jumbotron

--- run.py
+++ run.py
@@ -171,7 +171,7 @@ def edit_recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template(
-        "view_recipe.html", recipe=recipe, categories=categories)
+        "edit_recipe.html", recipe=recipe, categories=categories)
 
 
 @app.route("/delete_recipe/<recipe_id>")
